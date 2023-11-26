873d2aad61c56c234ae65a3393ec1e3adc3b9003
Xing Wu <41498567+silverlobster@users.noreply.github.com>
2023-11-26 15:26:27 -0500

    added movie reccommendations

--- test.php
+++ test.php
@@ -1,13 +1,51 @@
 <?php
 session_start();
 require "db_connect.php";
+?>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title> Home Page</title>
+        <style>
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand mx-3" href="#">
+                <?php echo "Hello " . $_SESSION["user"]?>! Welcome to Movie Recommender</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="test.php">Recomendations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Log Out</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+<html>
 
-// $movie_dict = array();
-// $sql = "SELECT movie_id, title FROM movies";
-// $result = $connect->query($sql);
-// while ($row = $result->fetch_assoc()) {
-//     $movie_dict[$row['movie_id']] = $row['title'];
-// }
+<?php
+$movie_dict = array();
+$sql = "SELECT movie_id, title FROM movies";
+$result = $connect->query($sql);
+ while ($row = $result->fetch_assoc()) {
+     $movie_dict[$row['movie_id']] = $row['title'];
+}
+//print_r($movie_dict);
 
 $ratings_dict = array();
 $sql = "SELECT user_id, movie_id, ratings FROM ratings";
@@ -20,8 +58,8 @@ $sim_dict = array();
 $sql = "SELECT * FROM users WHERE user_id != " . $_SESSION['uid'];
 $result = $connect->query($sql);
 
-print_r($ratings_dict);
-echo "" . "<br>";
+//print_r($ratings_dict);
+//echo "" . "<br>";
 while ($row = $result->fetch_assoc()) {
     $compared_user = $row['user_id'];
     $compared_user_average_rating = 0;
@@ -35,15 +73,15 @@ while ($row = $result->fetch_assoc()) {
     $newResult = $connect->query($sql);
     //print_r($newResult);
     //echo "" . "<br>";
-    print($compared_user);
-    echo "" . "<br>";
+    //print($compared_user);/
+    //echo "" . "<br>";
     while($common_movies = $newResult->fetch_assoc()) {
         //if there is a common movie in between two users, calculate the values to get user CF
-        print_r($common_movies);
-        echo "" . "<br>";
+        //print_r($common_movies);
+        //echo "" . "<br>";
         //print(isset($ratings_dict[$compared_user][$common_movies['movie_id']]));
         if (isset($ratings_dict[$compared_user][$common_movies['movie_id']])) {
-            print($num_same_movies);   
+            //print($num_same_movies);   
             $num_same_movies += 1;
             $user_average_rating += $ratings_dict[$_SESSION['uid']][$common_movies['movie_id']];
             $compared_user_average_rating += $ratings_dict[$compared_user][$common_movies['movie_id']];
@@ -71,8 +109,8 @@ while ($row = $result->fetch_assoc()) {
         $sim_dict[$compared_user] = 0;
     }
 }
-//print_r($sim_dict);
-//echo "done </br>";
+print_r($sim_dict);
+echo "Similarity Matrix Returned. </br>";
 function positive($var) {
     if ($var > 0) {
         return true;
@@ -107,4 +145,15 @@ while ($row = $result->fetch_assoc()) {
 // print_r($recommendations);
 arsort($recommendations);
 print_r(array_slice($recommendations, 0, 10, TRUE));
+echo "<br>";
+?>
+
+<?php
+    echo "Your Movie Recommendations are: <br>";
+    foreach (array_slice($recommendations, 0, 10, TRUE) as $movie_key => $sim_value) {
+        if ($sim_value > 3) {
+            echo $movie_dict[$movie_key] . "<br>";
+        }
+    }
+     
 ?>
